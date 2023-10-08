8279a92fe4f3d6d532bfdddcc66b07b1ccd24246
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-10-08 15:54:07 +0200

    First commit

---
+++ src/TwitterCopilotServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Brunocfalcao\LaravelHelpers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TwitterCopilotServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
